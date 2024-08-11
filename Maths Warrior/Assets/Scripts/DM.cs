47a49dfa75e9071e05e139772f6bdbb1e93530af
Ashish-Kr-1 <ashishkumar.ab01@gmail.com>
2024-08-12 02:18:06 +0530

    Added some UI elements

---
+++ Maths Warrior/Assets/Scripts/DM.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DM : MonoBehaviour
+{
+    public static string inputAnswer = "";
+
+    public static int answer ;
+
+    public static int level ;
+
+
+}
--- Maths Warrior/Assets/Scripts/GameManager.cs
+++ Maths Warrior/Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
     public Animator anm;
+    public TMP_Text answer;
 
     Rigidbody rb;
     public float speed;
@@ -15,21 +18,44 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         rb = player.GetComponent<Rigidbody>();
+
+        DM.inputAnswer = "";
+        DM.level = 1;
+        DM.answer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        answer.text = DM.inputAnswer;
     }
 
-    public void onClick(){
+    public void onClick()
+    {
         //Vector3 currentPos = player.transform.position;
 
         rb.AddForce(0, speed, 0, ForceMode.Impulse);
 
-        anm.SetBool("jumpRight", turn%2==0);
+        anm.SetBool("jumpRight", turn % 2 == 0);
 
         turn++;
+    }
+
+    public void numClick(Button btn)
+    {
+        DM.inputAnswer = DM.inputAnswer + btn.gameObject.tag;
+    }
+
+    public void setQuestion()
+    {
+        if (DM.level == 1)
+        {
+        }
+        else if (DM.level == 2)
+        {
+        }
+        else if (DM.level == 3)
+        {
+        }
     }
 }
